d70b526704e0a1dffa5cdb27646b0b19089ae305
Inzomnium <96298000+Inzomnium@users.noreply.github.com>
2024-01-23 13:11:44 +0100

    skillset y fixes de aboutme.js

--- src/routes/aboutme.js
+++ src/routes/aboutme.js
@@ -2,6 +2,7 @@ import { NavBar } from "../components/Navbar"
 import { Col, Container, Row, Image } from "react-bootstrap"
 import { motion } from "framer-motion"
 import { Footer } from "../components/footer"
+import { SkillSet } from "../components/SkillSet"
 
 export default function About () {
 
@@ -40,16 +41,32 @@ initial={{opacity:0 }}
 <section id="about-main">
     <Container fluid>
     <Row>
-    <Col lg={4} className="p-xs-3 p-md-0">
+    <Col lg={4} className="sidebar">
         <Image className="w-100" src="https://placehold.co/500x500" />
+        <div className="skillset">
+            <h5>Some of my skillset</h5>
+            <SkillSet />
+           
+
+        </div>
     </Col>
-    <Col lg={8} className="px-5">
-    <h2>Hello, I am Sebastian</h2>
-    <small className="small">Fermentum iaculis eu non diam phasellus vestibulum lorem sed.</small>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim tortor at auctor urna. Diam donec adipiscing tristique risus nec. Nec nam aliquam sem et tortor consequat id porta. Id consectetur purus ut faucibus pulvinar elementum integer enim neque. Tristique senectus et netus et malesuada fames. In iaculis nunc sed augue lacus. At auctor urna nunc id cursus metus aliquam eleifend. Diam quis enim lobortis scelerisque fermentum. Quisque egestas diam in arcu cursus.</p>
-    <h3>List of Skills and habilities</h3>
-<p>Vitae auctor eu augue ut lectus arcu bibendum at varius. Vitae nunc sed velit dignissim sodales ut. Id ornare arcu odio ut sem nulla pharetra diam. Ipsum nunc aliquet bibendum enim. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Id consectetur purus ut faucibus pulvinar elementum integer enim. Porttitor rhoncus dolor purus non enim praesent elementum. Maecenas ultricies mi eget mauris pharetra et. Vel fringilla est ullamcorper eget nulla facilisi etiam. Aliquam malesuada bibendum arcu vitae. Posuere ac ut consequat semper viverra nam. A diam sollicitudin tempor id eu. Donec enim diam vulputate ut pharetra sit amet. Sed libero enim sed faucibus turpis in eu mi bibendum. Sed adipiscing diam donec adipiscing tristique risus. Posuere sollicitudin aliquam ultrices sagittis. Id donec ultrices tincidunt arcu. Mollis nunc sed id semper risus in hendrerit. Id cursus metus aliquam eleifend mi in nulla. Pellentesque adipiscing commodo elit at.</p>
-<p>Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Integer eget aliquet nibh praesent tristique. Porttitor leo a diam sollicitudin tempor. Id volutpat lacus laoreet non curabitur gravida arcu ac tortor. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar. Massa id neque aliquam vestibulum morbi. Integer enim neque volutpat ac tincidunt vitae semper quis. Quisque non tellus orci ac auctor. Facilisi etiam dignissim diam quis. Hendrerit dolor magna eget est lorem ipsum dolor. Turpis tincidunt id aliquet risus. At in tellus integer feugiat scelerisque varius morbi. In nulla posuere sollicitudin aliquam ultrices sagittis orci a. Facilisis mauris sit amet massa vitae tortor condimentum lacinia. Neque egestas congue quisque egestas diam in arcu cursus. Ante metus dictum at tempor commodo.</p>
+    <Col lg={8} className="px-5 main">
+    <h2>Greetings! I'm Sebastián Cofré Barrientos</h2>
+    
+    <p>I am a seasoned <b>web developer</b> and <b>project manager</b> with over a decade of experience in steering the digital realm. My journey has been marked by a passion for crafting seamless online experiences, combining technical expertise with creative flair.</p>
+    <h4>Technical Mastery:</h4>
+<p>As a web enthusiast, I've mastered a spectrum of technologies, from the latest in <b>ReactJS</b> to the enduring efficiency of <b>jQuery</b>. My work is a testament to the seamless integration of cutting-edge solutions and proven methodologies.</p>
+ <h4>Versatility Unleashed:</h4>
+<p>My career spans across dynamic roles, from managing projects and leading development teams to providing insightful <b>consultancy on UX</b> and web design. Versatility isn't just a skill; it's a mindset that allows me to adapt and excel in diverse projects. Being a <b>quick learner</b> and adaptable to any system or CMS is not just a skill for me; it's a mindset that allows me to adapt and excel in diverse projects.</p>
+<h4>Project Management Prowess:</h4>
+<p>While my formal title might not scream "Project Manager," my experience <b>managing projects</b> and <b>teams speaks volumes</b>. I thrive in orchestrating projects, ensuring every element aligns with the vision, deadlines are met, and the end product exceeds expectations.</p>
+<h4>Client Collaboration:</h4>
+<p>I'm not just a developer; I'm a client's advocate. My knack for understanding client expectations, coupled with <b>clear communication</b> and a <b>user-centric approach</b>, fosters enduring relationships. In the collaborative dance of tech and creativity, I ensure every step is in tune with the client's vision.</p>
+<h4>What's Next?</h4>
+<p>Currently honing my skills in <b>ReactJS</b>, I'm on a perpetual quest for excellence. Whether you need a robust web solution, a project brought to life, or a reliable team leader, I bring dedication, technical prowess, and a touch of creativity to every endeavor.</p>
+<p>Join me in navigating the ever-evolving landscape of digital innovation.</p>
+
+
 <Image className="d-flex m-auto" src="https://placekitten.com/300/300" />
 
     </Col>
